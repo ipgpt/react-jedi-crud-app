6613b348bec4c26d5d0b86225cc635525d022443
Petro Fedorenko <petewaitforletters@gmail.com>
2020-06-16 14:34:17 +0300

    build navbar with styles

--- src/App.js
+++ src/App.js
@@ -1,50 +1,57 @@
-import React from 'react';
+import React from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  NavLink,
 } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.css';
-import JEDI from './components/JEDI';
-import People from './components/People';
-import Planets from './components/Planets';
-import Starships from './components/Starships';
+import "bootstrap/dist/css/bootstrap.css";
+import JEDI from "./components/JEDI";
+import People from "./components/People";
+import Planets from "./components/Planets";
+import Starships from "./components/Starships";
 
 function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">JEDI</Link>
-            </li>
-            <li>
-              <Link to="/people">People</Link>
-            </li>
-            <li>
-              <Link to="/planets">Planets</Link>
-            </li>
-            <li>
-              <Link to="/starships">Starships</Link>
-            </li>
-          </ul>
+        <nav className="navbar navbar-expand-lg bg-dark">
+          <NavLink
+            className="navbar-brand m-2 p-2 text-white"
+            activeClassName="bg-primary"
+            to="/"
+            exact
+          >
+            JEDI
+          </NavLink>
+          <NavLink
+            className="m-2 p-2 text-white"
+            activeClassName="bg-primary"
+            to="/people"
+          >
+            People
+          </NavLink>
+          <NavLink
+            className="m-2 p-2 text-white"
+            activeClassName="bg-primary"
+            to="/planets"
+          >
+            Planets
+          </NavLink>
+          <NavLink
+            className="m-2 p-2 text-white"
+            activeClassName="bg-primary"
+            to="/starships"
+          >
+            Starships
+          </NavLink>
         </nav>
 
         <Switch>
-          <Route path="/people">
-            <People />
-          </Route>
-          <Route path="/planets">
-            <Planets />
-          </Route>
-          <Route path="/starships">
-            <Starships />
-          </Route>
-          <Route path="/">
-            <JEDI />
-          </Route>
+          <Route exact path="/" component={JEDI} />
+          <Route exact path="/people" component={People} />
+          <Route exact path="/planets" component={Planets} />
+          <Route exact path="/starships" component={Starships} />
         </Switch>
       </div>
     </Router>
